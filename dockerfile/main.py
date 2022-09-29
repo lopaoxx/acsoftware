03ae2d2e172855c82557cda91187b8594721d616
Lucas Cardoso Silva <100287410+LucaaasCS@users.noreply.github.com>
2022-09-29 20:52:29 -0300

    Add files via upload

---
+++ dockerfile/main.py
@@ -0,0 +1,43 @@
+import os
+from flask import Flask, render_template, request
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+app = Flask(__name__)
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'mudar123'
+app.config['MYSQL_DATABASE_DB'] = 'acimpacta'
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+mysql.init_app(app)
+
+@app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/gravar', methods=['POST','GET'])
+def gravar():
+  nome = request.form['nome']
+  email = request.form['email']
+  senha = request.form['senha']
+  if nome and email and senha:
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute('insert into login (nome, email, senha) VALUES (%s, %s, %s)', (nome, email, senha))
+    conn.commit()
+  return render_template('index.html')
+
+
+@app.route('/listar', methods=['POST','GET'])
+def listar():
+  conn = mysql.connect()
+  cursor = conn.cursor()
+  cursor.execute('select nome, email, senha from login')
+  data = cursor.fetchall()
+  conn.commit()
+  return render_template('lista.html', datas=data)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
